f1b21638a21b7e02340ec0f623899447fc53d1b1
Malena Muñoz <malena.munoz.dev@gmail.com>
2025-06-12 18:06:46 +0200

    Página de presentación

--- lifeflow-app/src/pages/Welcome.jsx
+++ lifeflow-app/src/pages/Welcome.jsx
@@ -13,7 +13,7 @@ import { useGoogleLogin } from "@react-oauth/google";
 import { motion } from "framer-motion";
 import { GoogleLogin } from "../services/Google";
 import { Notyf } from 'notyf';
-import '../assets/styles/scroll.css';
+import '../assets/styles/Welcome.css';
 
 export default function Welcome(props) {
     const notyf = new Notyf();
@@ -49,13 +49,12 @@ export default function Welcome(props) {
                     </div>
                 </motion.div>
             </div>
-            <div className="pattern-container"></div>
             <div className='container min-vh-100'>
                 <motion.div
                 initial={{ opacity: 0 }} 
                 whileInView={{ opacity: 1 }}  
                 transition={{ duration: 1 }}  
-                className='vh-100 d-flex flex-column align-items-center justify-content-center gap-3'>
+                className='d-flex flex-column align-items-center justify-content-center gap-3 p-5 rounded borde-azul'>
                     <h1 className='m-0 mt-2 fw-bold txt-rosa-oscuro'>¿Qué es LifeFlow?</h1>
                     <h4>LifeFlow es una aplicación web destinada al registro y control de los ciclos menstruales femeninos.</h4>
                     <ul style={{fontSize: '25px'}}>
@@ -65,50 +64,55 @@ export default function Welcome(props) {
                         <li>Modo embarazo</li>
                         <li>Generación y exportación de reportes personalizados</li>
                     </ul>
-                    <div className='d-flex align-items-center gap-4 w-100'>
-                        <img src={IndexPNG} alt="" className='w-50'/>
-                        <img src={RecordatoriosPNG} alt="" className='w-50'/>
-                    </div>
-                </motion.div>
-            </div>
-            <div className="pattern-container"></div>
-            <div className='container'>
-                <motion.div
-                initial={{ opacity: 0 }} 
-                whileInView={{ opacity: 1 }}  
-                transition={{ duration: 1 }}  
-                className='d-flex flex-column align-items-center justify-content-center gap-5 py-5'>
-                    <h3 className='m-0 mt-2 fw-bold'>Tecnologías</h3>
-                    <div className='d-flex align-items-center gap-5 w-100'>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={ReactSVG} alt="" style={{height: '150px'}}/>
-                            <h5>React</h5>
+                    <div class="row g-2 w-100">
+                        <div class="col-6">
+                            <img src={IndexPNG} alt="" className='img-fluid w-100'/>
                         </div>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={NetCoreSVG} alt="" style={{height: '150px'}}/>
-                            <h5>.NET Core</h5>
-                        </div>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={SqlServerSVG} alt="" style={{height: '150px'}}/>
-                            <h5>SQL Server</h5>
-                        </div>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={BootstrapSVG} alt="" style={{height: '150px'}}/>
-                            <h5>Bootstrap</h5>
-                        </div>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={SassSVG} alt="" style={{height: '150px'}}/>
-                            <h5>SASS</h5>
-                        </div>
-                        <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
-                            <img src={GoogleSVG} alt="" style={{height: '150px'}}/>
-                            <h5>Google</h5>
+                        <div class="col-6">
+                            <img src={RecordatoriosPNG} alt="" className='img-fluid w-100'/>
                         </div>
                     </div>
                 </motion.div>
             </div>
-            <div className="pattern-container"></div>
-            
+            <div className='min-vh-100 d-flex flex-column align-items-center justify-content-around'>
+                <div className="pattern w-100" style={{height: '50px'}}></div>
+                <div className='container'>
+                    <motion.div
+                    initial={{ opacity: 0 }} 
+                    whileInView={{ opacity: 1 }}  
+                    transition={{ duration: 1 }}  
+                    className='d-flex flex-column align-items-center justify-content-center gap-5 py-5'>
+                        <h1 className='m-0 mt-2 fw-bold txt-rosa-oscuro'>Tecnologías</h1>
+                        <div className='d-flex align-items-center gap-5 w-100'>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={ReactSVG} alt="" style={{height: '150px'}}/>
+                                <h5>React</h5>
+                            </div>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={NetCoreSVG} alt="" style={{height: '150px'}}/>
+                                <h5>.NET Core</h5>
+                            </div>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={SqlServerSVG} alt="" style={{height: '150px'}}/>
+                                <h5>SQL Server</h5>
+                            </div>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={BootstrapSVG} alt="" style={{height: '150px'}}/>
+                                <h5>Bootstrap</h5>
+                            </div>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={SassSVG} alt="" style={{height: '150px'}}/>
+                                <h5>SASS</h5>
+                            </div>
+                            <div className='flex-grow-1 d-flex flex-column gap-3 align-items-center'>
+                                <img src={GoogleSVG} alt="" style={{height: '150px'}}/>
+                                <h5>Google</h5>
+                            </div>
+                        </div>
+                    </motion.div>
+                </div>
+                <div className="pattern w-100" style={{height: '50px'}}></div>
+            </div>
         </>
     );
 }
